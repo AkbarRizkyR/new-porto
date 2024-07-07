b2dc73f1e475140d7b4ef75faad02c0382aaf093
Arrizki0098 <akbar.rabbani@iconpln.co.id>
2024-07-07 23:10:46 +0700

    Fixing

--- app/page.jsx
+++ app/page.jsx
@@ -10,27 +10,41 @@ const Home = () => {
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
           <div className="text-center xl:text-left order-2 xl:order-none">
-            <span className="text-xl">
-              Front End Developer
-            </span>
+            <span className="text-xl">Front End Developer</span>
             <h1 className="h1 mb-6">
-            Hello Aku <br /> <span className="text-accent text-[60px]">Akbar Risky Rabbani</span>
+              Hello Aku <br />{" "}
+              <span className="text-accent text-[60px]">
+                Akbar Risky Rabbani
+              </span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
-              I am a Front End Developer based in Indonesia. I have rich experience in web site design and building,
-              also I am good at responsive design, web animations, and front-end libraries.
+              I am a Front End Developer based in Indonesia. I have rich
+              experience in web site design and building, also I am good at
+              responsive design, web animations, and front-end libraries.
             </p>
             {/* btn & sosial */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
-                <span>
-                  download cv
+              <a
+                href="https://drive.google.com/uc?export=download&id=1pDfVLWgIBQH84kMfbxhNvEQzv4kZ4C4f" // Replace FILE_ID with your actual file ID
+                target="_blank"
+                rel="noopener noreferrer"
+                className="uppercase flex items-center gap-2"
+              >
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="uppercase flex items-center gap-2"
+                >
+                  <span>download cv</span>
                   <FiDownload className="text-xl" />
-                </span>
-              </Button>
+                </Button>
+              </a>
               <div className="mb-8 xl:mb-0">
-                <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border-accent rounded-full flex
-                justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"/>
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border-accent rounded-full flex
+                justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
               </div>
             </div>
           </div>
@@ -43,6 +57,5 @@ const Home = () => {
     </section>
   );
 };
-
 
 export default Home;
--- components/Social.jsx
+++ components/Social.jsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 import { FaGithub, FaLinkedin, FaYoutube, FaTwitter } from "react-icons/fa";
 
 const socials = [
-    { icon: <FaGithub />, path: "" },
-    { icon: <FaLinkedin />, path: "" },
-    { icon: <FaYoutube />, path: "" },
-    { icon: <FaTwitter />, path: "" },
+    { icon: <FaGithub />, path: "https://github.com/AkbarRizkyR" },
+    { icon: <FaLinkedin />, path: "https://www.linkedin.com/in/akbar-rizky-rabbani-a66a20148/" },
+    { icon: <FaYoutube />, path: "https://www.youtube.com/channel/UCMOukWZhRPoGIuC64mvso3A" },
+    { icon: <FaTwitter />, path: "https://twitter.com/Akbarrizki0098" },
 ];
 
 const Social = ({ containerStyles, iconStyles }) => {
